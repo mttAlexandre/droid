be72bf12ae725688ac5d7bf344d415fbd5b795f6
jaimelepoulet <alexandre.montcuit@icloud.com>
2018-11-30 18:11:45 +0100

    adaptater avance

--- app/src/main/java/com/example/alex/droid/Home.java
+++ app/src/main/java/com/example/alex/droid/Home.java
@@ -6,11 +6,16 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -18,19 +23,13 @@ import java.util.List;
 import java.util.Random;
 
 public class Home extends AppCompatActivity { // test push
-
-    int testpush=0;
+    
     private DBTache dbt;
     private ListView hlv;
-    private String[] prenoms = new String[]{
-            "Antoine", "Benoit", "Cyril", "David", "Eloise", "Florent",
-            "Gerard", "Hugo", "Ingrid", "Jonathan", "Kevin", "Logan",
-            "Mathieu", "Noemie", "Olivia", "Philippe", "Quentin", "Romain",
-            "Sophie", "Tristan", "Ulric", "Vincent", "Willy", "Xavier",
-            "Yann", "Zoé"
-    };
     private ArrayAdapter<String> adapter;
     private boolean checkbox=false;
+
+    MyCustomAdapter dataAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -129,6 +128,10 @@ public class Home extends AppCompatActivity { // test push
         //aller à la page détail et afficher l'item cliqué
     }
 
+
+
+
+
     private class MyCustomAdapter extends ArrayAdapter<Tache>{
 
         private ArrayList<Tache> tacheList;
@@ -143,5 +146,77 @@ public class Home extends AppCompatActivity { // test push
             TextView code;
             CheckBox name;
         }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+            Log.v("ConvertView", String.valueOf(position));
+
+            if (convertView == null) {
+                LayoutInflater vi = (LayoutInflater)getSystemService(
+                        Context.LAYOUT_INFLATER_SERVICE);
+                convertView = vi.inflate(R.layout.checkitem, null);
+
+                holder = new ViewHolder();
+                holder.code = (TextView) convertView.findViewById(R.id.code);
+                holder.name = (CheckBox) convertView.findViewById(R.id.checkBox1);
+                convertView.setTag(holder);
+
+                holder.name.setOnClickListener( new View.OnClickListener() {
+                    public void onClick(View v) {
+                        CheckBox cb = (CheckBox) v ;
+                        Tache tache = (Tache) cb.getTag();
+                        Toast.makeText(getApplicationContext(),
+                                "Clicked on Checkbox: " + cb.getText() +
+                                        " is " + cb.isChecked(),
+                                Toast.LENGTH_LONG).show();
+                        tache.setChecked(cb.isChecked());
+                    }
+                });
+            }
+            else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            Tache tache = tacheList.get(position);
+            holder.code.setText(" (" +  tache.getNom() + ")");
+            holder.name.setText(tache.getNom());
+            holder.name.setChecked(tache.getChecked());
+            holder.name.setTag(tache);
+
+            return convertView;
+        }
+
+
+
     }
+
+    /*private void checkButtonClick() {
+
+
+        Button myButton = (Button) findViewById(R.id.button3);
+        myButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                StringBuffer responseText = new StringBuffer();
+                responseText.append("The following were selected...\n");
+
+                ArrayList<Tache> countryList = dataAdapter.tacheList;
+                for(int i=0;i<countryList.size();i++){
+                    Tache tache = countryList.get(i);
+                    if(tache.getChecked()){
+                        responseText.append("\n" + tache.getNom());
+                    }
+                }
+
+                Toast.makeText(getApplicationContext(),
+                        responseText, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }*/
 }
