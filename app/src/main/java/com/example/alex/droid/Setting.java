092e79d08f67bb447df35bf55fead80a2f37e13a
jaimelepoulet <alexandre.montcuit@icloud.com>
2018-12-01 10:40:20 +0100

    Page settings

--- app/src/main/java/com/example/alex/droid/Setting.java
+++ app/src/main/java/com/example/alex/droid/Setting.java
@@ -4,15 +4,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioGroup;
 
 // MAXIME : "PAGE DEU SETTINGS" BREF ON VERRA APRES POUR LE DESIGNE ET LES DIFFERENTES MODIF
 
 public class Setting extends Activity {
 
+    RadioGroup radio;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting);
+
+        radio=findViewById(R.id.radio);
+
     }
 
     public void onClickHome(View v){
@@ -21,7 +27,7 @@ public class Setting extends Activity {
     }
 
     public void onClickValider(View v){
-        
+
 
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
